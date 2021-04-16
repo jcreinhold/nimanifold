a505823346ec7ab265a408666f6614bc6fef09e4
Jacob Reinhold <jacob.reinhold@jhu.edu>
2021-04-16 11:47:46 -0400

    add minmax scaler to plot for more consistency

--- nimanifold/plot/generic.py
+++ nimanifold/plot/generic.py
@@ -20,6 +20,7 @@ from typing import *
 from matplotlib import offsetbox
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import minmax_scale
 
 from nimanifold.types import *
 
@@ -38,33 +39,37 @@ def _get_color(samples: Sample, name: str):
 def plot(samples: Sample,
          colors: Optional[str] = None,
          ax: Axes = None,
-         title: str = None) -> None:
+         title: str = None,
+         scale: bool = True) -> None:
     if colors is not None:
         colors = _get_color(samples, colors)
+    data = samples.data
+    if scale:
+        data = minmax_scale(data)
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=(8, 8))
-    ax.scatter(samples.data[:, 0], samples.data[:, 1], c=colors, s=3.)
+    ax.scatter(data[:, 0], data[:, 1], c=colors, s=3.)
     ax.set_facecolor("black")
     ax.axis('scaled')
     ax.xaxis.set_tick_params(**TICK_PARAMS)
     ax.yaxis.set_tick_params(**TICK_PARAMS)
     if samples.slices is not None:
-        scatter_imgs(samples.data, samples.slices, ax)
+        scatter_imgs(data, samples.slices, ax)
     if title is not None:
         plt.title(title)
 
 
-def scatter_imgs(data: Array, slices: Array, ax: Axes):
+def scatter_imgs(data: Array, slices: Array, ax: Axes, eps: float = 4e-3):
     if hasattr(offsetbox, 'AnnotationBbox'):
         # only print thumbnails with matplotlib > 1.0
         shown_images = np.array([[1., 1.]])  # just something big
         for i in range(data.shape[0]):
             dist = np.sum((data[i] - shown_images) ** 2, 1)
-            if np.min(dist) < 4e-3:
+            if np.min(dist) < eps:
                 # don't show points that are too close
                 continue
             shown_images = np.r_[shown_images, [data[i]]]
             imagebox = offsetbox.AnnotationBbox(
-                offsetbox.OffsetImage(slices[i], cmap=plt.cm.gray_r),
+                offsetbox.OffsetImage(slices[i], cmap=plt.cm.gray),
                 data[i])
             ax.add_artist(imagebox)
